66330621dfb884fda4fc8506e1b8fa37fe465050
seyi103 <seyi607@gmail.com>
2022-09-14 16:24:12 +0900

    Create merge_sort.py
    병합 정렬

---
+++ merge_sort.py
@@ -0,0 +1,43 @@
+#주어진 배열을 원소가 하나 밖에 남지 않을 때까지 계속 둘로 쪼갠 후에 다시 크기 순으로 재배열 하면서 원래 크기의 배열로 만듦
+
+def merge_sort(arr):
+    def sort(low, high):
+        if high - low < 2:
+            return
+        mid = (low + high) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        temp = []
+        l, h = low, mid
+
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < high:
+            temp.append(arr[h])
+            h += 1
+
+        for i in range(low, high):
+            arr[i] = temp[i - low]
+
+    return sort(0, len(arr))
+
+arr = [7, 6, 5, 8, 3, 5, 9, 1]
+merge_sort(arr)
+'''
+분할(split) 단계와 방합(merge) 단계
+O(logN) 시간
+시간 복잡도는 O(NlogN)
+시간 복잡도는 O(NlogN)
+'''
