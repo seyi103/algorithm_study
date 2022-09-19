fbb9161f250cf3d576002129c58b3ac98ac22687
seyi103 <seyi607@gmail.com>
2022-09-19 15:13:55 +0900

    Create b10989.py
    백준 10989

---
+++ coding_test/b10989.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+num_list = [0] * 10001
+
+for _ in range(n):
+    num_list[int(input())] += 1
+
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]):
+            print(i)
